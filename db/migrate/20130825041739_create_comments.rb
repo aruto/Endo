1195a9a5bb0d6beb7ee3ce12ac8a9af3c58910f4
Hokuto Kon <bei_north@yahoo.co.jp>
2013-08-26 10:19:28 +0900

    6.1終了 マイグレーションのタイプミス 注意しましょう

--- db/migrate/20130825041739_create_comments.rb
+++ db/migrate/20130825041739_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.string :commenter
-      t.next :body
+      t.text :body
       t.references :post, index: true
 
       t.timestamps
